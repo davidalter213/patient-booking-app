220102e2459bc71dcf24f05b6ce833ccfd0c8eef
David Alter <dalter4@uwo.ca>
2024-10-06 20:38:12 -0400

    Finished Homepage + user authentication

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import {
     Form,
     FormControl,
@@ -8,9 +9,13 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
+import { E164Number } from "libphonenumber-js/core";
 import { Input } from "@/components/ui/input"
 import { Control } from "react-hook-form"
 import { FormFieldType } from "./forms/PatientForm"
+import Image from "next/image"
+import 'react-phone-number-input/style.css'
+import PhoneInput from "react-phone-number-input/input"
 
 interface CustomProps {
     control: Control<any>,
@@ -28,12 +33,47 @@ interface CustomProps {
 }
 
 const RenderField = ({field, props}: {field: any; props: CustomProps}) => {
-    return (
-        <Input
-        type = "text"
-        placeholder="John Doe"
-        />
-    )
+   const { fieldType, iconSrc, iconAlt, placeholder} = props;
+
+   switch (fieldType){
+        case FormFieldType.INPUT:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    {iconSrc && (
+                        <Image
+                            src={iconSrc} 
+                            height={24}
+                            width={24} 
+                            alt={iconAlt || 'icon'}
+                            className="ml-2"
+                        />
+                    )}
+                    <FormControl>
+                        <Input
+                            placeholder={placeholder}
+                            {...field}
+                            className="shad-input border-0"
+                        />
+                    </FormControl>
+                </div>
+            )
+        case FormFieldType.PHONE_INPUT:
+            return (
+                <FormControl>
+                    <PhoneInput
+                        defaultCountry="US"
+                        placeholder = {placeholder}
+                        international
+                        withCountryCallingCode
+                        value={field.value as E164Number | undefined}
+                        onChange={field.onChange}
+                        className = "input-phone"
+                    />
+                </FormControl>
+            )
+        default:
+            break;
+   }
 }
 
 const CustomFormField = (props : CustomProps) => {
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -7,6 +7,10 @@ import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -18,23 +22,33 @@ export enum FormFieldType {
     SKELETON = 'skeleton', 
 }
  
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
  
 const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false)
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
  
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try{
+      // const userData = {name, email, phone};
+      // const user = await createUser(userData);
+
+      // if(user) router.push(`/patients/${user.$id}/register`)      
+    } catch (error) {
+      console.log(error)
+    }
   }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
@@ -53,9 +67,25 @@ const PatientForm = () => {
             iconAlt = "user"
         />
 
-       
-        
-        <Button type="submit">Submit</Button>
+        <CustomFormField
+            fieldType = {FormFieldType.INPUT}
+            control = {form.control}
+            name = "email"
+            label = "Email"
+            placeholder = "example@gmail.com"
+            iconSrc = "/assets/icons/email.svg"
+            iconAlt = "email"
+        />
+
+        <CustomFormField
+            fieldType = {FormFieldType.PHONE_INPUT}
+            control = {form.control}
+            name = "phone"
+            label = "Phone Number"
+            placeholder = "(555) 123-4567"
+          />
+
+        <SubmitButton isLoading = {isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   )
